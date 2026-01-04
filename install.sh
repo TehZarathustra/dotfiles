b4411a6562d145cda51b74158ac032c3f59012d8
TehZarathustra <tehzarathustra@gmail.com>
2026-01-04 15:52:19 +0100

    Adds todo

--- install.sh
+++ install.sh
@@ -2,9 +2,12 @@
 set -eu
 
 # portable on macOS + Ubuntu + Arch
-# NOTE: Assumes this script is run via ./install.sh (not via PATH);
+# NOTE: assumes this script is run via ./install.sh (not via PATH);
 # if invoked without a slash, $0 may not resolve to the script directory
 ROOT=$(cd "$(dirname "$0")" && pwd)
+
+# @TODO add guards
+# install script can silently overwrite real files
 
 # home
 ln -snf "$ROOT/home/.gitconfig" "$HOME/.gitconfig"
