f84111692e5fcd88730498d9c08391b6d546e24e
TehZarathustra <tehzarathustra@gmail.com>
2026-07-27 10:13:18 +0200

    Updates treesitter conf

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -1,16 +1,21 @@
 return {
     'nvim-treesitter/nvim-treesitter',
+    -- TODO: this conf using deprecated API, gotta move to new api
+    -- https://github.com/nvim-treesitter/nvim-treesitter/discussions/7927
+    -- https://github.com/nvim-treesitter/nvim-treesitter/discussions/7901
+    -- for now specifying master branch is enough to keep the current conf afloat
+    branch = 'master',
     priority = 100,
     config = function()
         require('nvim-treesitter.configs').setup {
             -- Install these parsers by default
-            ensure_installed = { 
-                'lua', 'vim', 'vimdoc', 'javascript', 'typescript', 'python', 'haskell', 
-                'html', 'scss', 'css', 'json', 'yaml', 'toml', 'markdown', 'bash' 
+            ensure_installed = {
+                'lua', 'vim', 'vimdoc', 'javascript', 'typescript', 'python', 'haskell',
+                'html', 'scss', 'css', 'json', 'yaml', 'toml', 'markdown', 'bash'
             },
             -- Automatically install missing parsers
             -- (i.e i've opened a file and don't have parser for it)
-            auto_install = true, 
+            auto_install = true,
             highlight = {
                 enable = true,
                 additional_vim_regex_highlighting = false,
